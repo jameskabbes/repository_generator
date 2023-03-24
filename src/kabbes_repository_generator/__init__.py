10f88ccde1bb006732934de6d349690ec289a1cc
James Kabbes <james.kabbes@gmail.com>
2023-03-23 22:38:15 -0500

    bringing user_profile back

--- src/kabbes_repository_generator/__init__.py
+++ src/kabbes_repository_generator/__init__.py
@@ -1,5 +1,6 @@
 import os
 import dir_ops as do
+import user_profile #not explicitly used, but needed for reference
 
 _Dir = do.Dir( os.path.abspath( __file__ ) ).ascend()   #Dir that contains the package 
 
